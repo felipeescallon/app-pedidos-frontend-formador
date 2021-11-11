1dafcea88bfdc191f78d47d0677eacdf3926a157
Andres Felipe Escallon Portilla <aescallon@unicauca.edu.co>
2021-11-10 23:44:16 -0500

    second commit

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { ValidadorSesionGuard } from 'src/app/guardianes/validador-sesion.guard';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto/editar-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-persona',
+    component: CrearPersonaComponent
+  },
+  {
+    path: 'editar-persona',
+    component: EditarPersonaComponent,
+    //canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: "listar-productos",
+    component: BuscarProductoComponent,
+    //canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: 'crear-producto',
+    component: CrearProductoComponent,
+    //canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: 'editar-producto/:id',
+    component: EditarProductoComponent,
+    //canActivate: [ValidadorSesionGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
