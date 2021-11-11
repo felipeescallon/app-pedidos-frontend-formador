e93f0847a277867a8e9a2b348b39ee89f9fcf833
Andres Felipe Escallon Portilla <aescallon@unicauca.edu.co>
2021-11-11 14:53:09 -0500

    CRUD funcionando-fin video 6.2

--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -70,6 +70,16 @@ export class SeguridadService {
     return datosString;
   }
 
+  ObtenerToken(){
+    let datosString = localStorage.getItem("datosSesion");
+    if(datosString){
+      let datos = JSON.parse(datosString);
+      return datos.tk;
+    }else{
+      return '';
+    }
+  }
+
 
 
 
